075c32d4cab3c587566c14347a2a5226fc4ace40
Jimbo77 <chupyalcohol@gmail.com>
2017-03-22 16:37:01 -0600

    Fixes after B19 Sources Update

--- drivers/soc/qcom/pil-q6v5-mss.c
+++ drivers/soc/qcom/pil-q6v5-mss.c
@@ -32,7 +32,7 @@
 #include <soc/qcom/ramdump.h>
 #include <soc/qcom/smem.h>
 #include <soc/qcom/smsm.h>
-#include <soc/qcom/vendor/modem_fatal_error.h>
+/*#include <soc/qcom/vendor/modem_fatal_error.h>*/
 
 #include "peripheral-loader.h"
 #include "pil-q6v5.h"
@@ -64,7 +64,7 @@ static void log_modem_sfr(void)
 	strlcpy(reason, smem_reason, min(size, MAX_SSR_REASON_LEN));
 	pr_err("modem subsystem failure reason: %s.\n", reason);
 
-	modem_fatal_error_update_reason(smem_reason);
+	/*modem_fatal_error_update_reason(smem_reason);*/
 
 	smem_reason[0] = '\0';
 	wmb();
